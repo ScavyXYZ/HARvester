386ffb5579d0c66eb48ce8d77223588bc3a94a38
ScavyXYZ <ScavyXYZ@protonmail.com>
2025-10-07 23:48:20 +0300

    Update harRequest.cpp

--- examples/harRequest.cpp
+++ examples/harRequest.cpp
@@ -6,20 +6,19 @@ int main()
 {
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
-    // HAR file example
     //open file
     std::ifstream file("request.har");
     if (!file) {
         std::cout << "ERROR: Cannot open request.har" << std::endl;
         return 1;
     }
+    
     //creating request obj
     request* r = new request(file);
 
     //sending request
     r->send();
-
-    std::cout << "HAR: ";
+    
     //if success
     if (r->isOK())
     {
